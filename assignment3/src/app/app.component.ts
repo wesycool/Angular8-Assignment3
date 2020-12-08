5e02d8bf51027ac85614ca91a3c4329f4845dcfa
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-12-08 13:22:30 -0500

    Added forms

--- assignment3/src/app/app.component.ts
+++ assignment3/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment3';
+  defaultSubscription = 'Advanced'
+
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 }
